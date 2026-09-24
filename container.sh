a6b01ab352bba139b15eefd868214a8bb356db5b
Student <student@teknologi2.local>
2026-09-24 10:25:57 +0200

    added my-nginx-2 to container.sh

--- container.sh
+++ container.sh
@@ -2,7 +2,9 @@
 set -e
 
 docker run -d --name my-nginx nginx
-docker run -d --name second-nginx
 docker ps
 docker exec my-nginx ls -l /usr/share/nginx/html
 docker logs my-nginx
+
+docker run -d --name my-nginx-2 nginx
+docker ps
