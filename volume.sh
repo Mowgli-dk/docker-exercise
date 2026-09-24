e930cabf978baedc5fdf594dcdde33fcec00e324
Student <student@teknologi2.local>
2026-09-24 11:30:34 +0200

    volume.sh added

---
+++ volume.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+docker volume create minvolume
+
+docker run --rm -v minvolume:/data alpine sh -c 'echo "Hej fra en volume!" > /data/besked.txt'
+
+docker run --rm -v minvolume:/data alpine cat /data/besked.txt
+
+docker volume rm minvolume
